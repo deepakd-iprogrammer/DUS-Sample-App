5ee1900c2cb2ce171069f5adec5c91af261e7be2
deepak d <deepakd@iprogrammer.com>
2019-05-07 13:47:15 +0530

    test

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Run sh deploy2.sh</Text>
-        <Text>Relaunch your app once the update is deployed</Text>
-        <Text>Your new changes should reflect now</Text>
+       
+        <Text>Welcome</Text>
+        <Button title="submit" />
       </View>
     );
   }
